1994ce278acd3d5ba7450ece1e905031d2090a01
Daniel <wndtn853@gmail.com>
2017-10-11 22:16:06 +0900

    Initial Commit

---
+++ src/chapter2/2_14_DataClassExam1.kt
@@ -0,0 +1,10 @@
+package chapter2
+
+data class Book(val title:String="No Title",
+                val author:String="No Author",
+                val price:Int=0)
+
+fun main(args:Array<String>) {
+    val book = Book()
+    println("${book.title} / ${book.author} / ${book.price}")
+}
---
+++ src/chapter2/2_14_DataClassExam3.kt
@@ -0,0 +1,17 @@
+package chapter2
+/*
+data class Book(val title:String="", val author:String="", var price:Int=0)
+
+fun main(args:Array<String>) {
+    val book = Book("Kotlin", "Daniel", 10000)
+    println("${book.title} / ${book.author} / ${book.price}")
+
+    val book2 = book.copy(price = 20000)
+    println("${book2.title} / ${book2.author} / ${book2.price}")
+
+    println(book == book2)
+
+    book2.price = 30000
+    println("${book2.title} / ${book2.author} / ${book2.price}")
+}
+*/
